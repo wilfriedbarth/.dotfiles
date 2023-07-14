2e6f9fa0b0e4913b7299f6f2e6689b27b9d338e4
Wilfried Barth <wilfried.barth.prof@gmail.com>
2023-07-14 17:03:34 -0500

    Update nvim config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     -- import/override with your plugins
     { import = "plugins" },
   },
