c40227bee00a40115ebf94fd05fdbbd0a5cb8ceb
Wilfried Barth <wilfried.barth.prof@gmail.com>
2024-07-26 10:41:30 -0500

    Clean up plugins

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { "nvim-neotest/neotest-python" },
-  { "nvim-neotest/neotest-go" },
-  { "nvim-neotest/neotest-jest" },
-  {
-    "nvim-neotest/neotest",
-    opts = { adapters = { "neotest-python", "neotest-go", "neotest-jest" } },
-  },
-}
